604648506c26f1383e51941c25978ccfd7e79913
Hake Huang <hakehuang@gmail.com>
2012-06-30 21:32:13 +0800

    init infoflow
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infoflow::Application.config.secret_token = '3db8ca780b5b2dcc7d8ef86f9244afc5e4d590f12c4e53cc3e7a3bc4feec713e51b0821b08d8b6dbec7bd85be6c9a53f2fabdde8e4e5cfc871f1e12aad923193'
