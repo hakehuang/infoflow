b900a1dab65209a99662301a6b8fcfd970e5c81d
Hake Huang <hakehuang@gmail.com>
2012-07-07 18:44:45 +0800

    enable username login
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,29 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :lockable, :authentication_keys => [:login]
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username ,:email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  attr_accessible :login
+
+
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
+
+### This is the correct method you override with the code above
+### def self.find_for_database_authentication(warden_conditions)
+### end 
 end
