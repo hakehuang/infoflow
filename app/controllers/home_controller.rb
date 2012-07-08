15277895cff14f70225fe67e5965aa0785dd4e33
Hake Huang <hakehuang@gmail.com>
2012-07-08 10:27:25 +0800

    fix a bug for username sign up
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+before_filter :authenticate_user!
+
+def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  
+    end
+end
+
 end
