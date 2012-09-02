3c55e5820e8c8effb975d8d5ade2f4b61605fe0d
Hake Huang <hakehuang@gmail.com>
2012-09-02 21:31:14 +0800

    remove s
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'a9cdf23344357fd9652c8113f8a32c38b503a1d80777a3c45a30e3623354c62ea13511cffb851aa7f67ffce0028a3663c6734ff009933750dcd60a80f476f0c2';
