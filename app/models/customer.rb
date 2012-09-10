75c6dbf271226a05e9b75be81330325f29aa7fa9
Hake Huang <hakehuang@gmail.com>
2012-09-10 19:40:50 +0800

    add version control
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
   attr_accessible :Address, :Contactor_a, :Contactor_b, :Country, :Email, :Fax, :Invoicing_Requirement, :MSN, :Market, :Postal_Code, :Skype, :Tel, :cell_a, :cell_b, :city, :name, :shipping_Document_requirements, :web_page
+has_paper_trail
 end
