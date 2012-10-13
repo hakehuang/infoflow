7168bcf8e831c661a1210476dad21c18b39be1dc
Hake Huang <hakehuang@gmail.com>
2012-10-13 16:54:23 +0800

    add paper_tail
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   attr_accessible :certification, :name, :serial_no
   has_and_belongs_to_many :manufactures
   has_and_belongs_to_many :contracts
+  has_paper_trail
 end
