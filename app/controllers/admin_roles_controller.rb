d19cb37cf0b574ed64d9bbc9c57ccb566de00692
Hake Huang <hakehuang@gmail.com>
2012-10-31 21:16:17 +0800

    update admin roles seting
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/controllers/admin_roles_controller.rb
+++ app/controllers/admin_roles_controller.rb
@@ -10,8 +10,31 @@ def index
 end
 
 def update
-  @user_id = params[:user_id]
-  @relate = RolesUsers.where("user_id" + "LIKE :range", :range => "%" + @user_id + "%")
-end 
+  @user_id = params[:user][:id]
+  
+ params.values.each do |param|
+        if param == "check"
+            @nrelate = RolesUsers.new
+            @nrelate.user_id = params[:user][:id]
+            @nrelate.role_id = @role.id
+            @nrelate.save
+        end
+  end
 
+  @relates = RolesUsers.where("user_id" + "LIKE :range", :range => "%" + @user_id + "%")
+  @relates.each do |relate|
+    @role = Role.find(relate.role_id)
+     params.each do |param|
+        if @role.role == param.key and @param.value == "uncheck"
+            relate.destroy
+        end
+    end
+  end
+  
+  respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @projects }
+  end
+end
+ 
 end
