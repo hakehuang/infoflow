2b39a01a106e4bfed742597ce99d026fa6412852
Hake Huang <hakehuang@gmail.com>
2013-02-14 19:53:25 +0800

    add home page
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-#  def after_sign_in_path_for(resource)
-#	dashboard_index_path
-#  end
+  def after_sign_in_path_for(resource)
+	dashboard_index_path
+  end
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
